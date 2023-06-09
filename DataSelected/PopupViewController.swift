3da6c4e3ee4193f15bf438928d0d56e86c91298b
Aglarov <shamil.aglarov@gmail.com>
2023-06-09 18:12:20 +0300

    сделал окончательный рефакторинг кода исправил мелкие ошибки

--- DataSelected/PopupViewController.swift
+++ DataSelected/PopupViewController.swift
@@ -27,7 +27,7 @@ class PopupViewController: UIViewController {
         textView.layer.cornerRadius = 5
         textView.backgroundColor = UIColor.darkGray
         // Добавляем начальный текст
-        let mainString = "Вечеринка состоится "
+        let mainString = "Встреча состоится "
         let highlightString = "{date}"
         
         let attributedMainString = NSMutableAttributedString(string: mainString)
@@ -68,10 +68,12 @@ class PopupViewController: UIViewController {
         numberFormatter.locale = Locale(identifier: "ru_RU")
         
         var dateString = String()
+        var toYears = false
         
         if let years = components.year, years > 0 {
             let yearsEnding = (years % 10 == 1 && years % 100 != 11) ? "год" : (years % 10 >= 2 && years % 10 <= 4 && (years % 100 < 10 || years % 100 >= 20) ? "года" : "лет")
             dateString += "через \(numberFormatter.string(from: NSNumber(value: years))!) \(yearsEnding) "
+            toYears = true
         }
         
         if let months = components.month, months > 0 {
@@ -80,8 +82,14 @@ class PopupViewController: UIViewController {
         }
         
         if let days = components.day, days > 0 {
-            let daysEnding = (days % 10 == 1 && days % 100 != 11) ? "день" : (days % 10 >= 2 && days % 10 <= 4 && (days % 100 < 10 || days % 100 >= 20) ? "дня" : "дней")
-            dateString += "и \(numberFormatter.string(from: NSNumber(value: days))!) \(daysEnding) "
+            if toYears {
+                let daysEnding = (days % 10 == 1 && days % 100 != 11) ? "день" : (days % 10 >= 2 && days % 10 <= 4 && (days % 100 < 10 || days % 100 >= 20) ? "дня" : "дней")
+                dateString += "и \(numberFormatter.string(from: NSNumber(value: days))!) \(daysEnding) "
+            } else {
+                let daysEnding = (days % 10 == 1 && days % 100 != 11) ? "день" : (days % 10 >= 2 && days % 10 <= 4 && (days % 100 < 10 || days % 100 >= 20) ? "дня" : "дней")
+                dateString += "через \(numberFormatter.string(from: NSNumber(value: days))!) \(daysEnding) "
+            }
+            
         }
         
         if dateString.isEmpty {
